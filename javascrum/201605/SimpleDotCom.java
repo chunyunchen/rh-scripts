5feb41403453518ae5026115d872243802e825f6
roadsoar <chunchen@redhat.com>
2016-05-08 13:23:01 +0800

    update

--- javascrum/201605/SimpleDotCom.java
+++ javascrum/201605/SimpleDotCom.java
@@ -1,25 +1,25 @@
-public class SimpleDotCom {
-   private int[] locationCells;
-   private int numOfHits;
+import java.util.ArrayList;
 
-   public String checkYourself(String stringGuess) {
-      int guess = Integer.parseInt(stringGuess);
+public class SimpleDotCom {
+   private ArrayList<String> locationCells;
+
+   public String checkYourself(String userInput) {
       String result = "miss";
-      for (int cell: locationCells) {
-            if (guess == cell) {
-               result = "hit";
-               numOfHits++;
-               break;
-            }
-      }
-      if (numOfHits == locationCells.length) {
-         result = "kill";
+      int index = locationCells.indexOf(userInput);
+      if (index >= 0) {
+         result = "hit";
+         locationCells.remove(index);
+         if (locationCells.isEmpty()) {
+            result = "kill";
+         } else {
+           result = "hit";
+         }
       }
       System.out.println(result);
       return result;
    }
 
-   public void setLocationCells(int [] locas) {
+   public void setLocationCells(ArrayList<String> locas) {
       locationCells = locas;
    }
 }
--- javascrum/201605/SimpleDotComGame.java
+++ javascrum/201605/SimpleDotComGame.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
 public class SimpleDotComGame {
    public static void main(String [] args) {
       int numOfGuesses = 0;
       SimpleDotCom dot = new SimpleDotCom();
       GameHelper helper = new GameHelper();
       int randomNum = (int) (Math.random() * 5);
-      int[] locations = {randomNum,randomNum+1,randomNum+2};
+      ArrayList<String> locations = new ArrayList<String>();
+      locations.add(Integer.toString(randomNum));
+      locations.add(Integer.toString(randomNum+1));
+      locations.add(Integer.toString(randomNum+2));
       dot.setLocationCells(locations);
       boolean isAlive = true;
       while(isAlive) {
