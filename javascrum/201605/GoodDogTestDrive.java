f54b8081d897bce88f2b65812a718e76e6da34a0
roadsoar <chunchen@redhat.com>
2016-05-30 21:55:25 +0800

    update

--- javascrum/201605/GoodDogTestDrive.java
+++ javascrum/201605/GoodDogTestDrive.java
@@ -1,12 +1,32 @@
 class GoodDog {
    private int size;
+   private String name;
+   private String says;
    
    public int getSize() {
       return size;
    }
 
+   public GoodDog() {}
+   public GoodDog(String nm, String ss) {
+     name = nm;
+     says = ss;
+   }
+
    public void setSize(int s) {
      size = s;
+   }
+
+   public String getName() {
+     return name;
+   }
+
+   public boolean equals(GoodDog dog) {
+     return name.equals(dog.name) && says.equals(dog.says);
+   }
+
+   public String getSays() {
+     return says;
    }
 
    void bark() {
@@ -33,5 +53,31 @@ class GoodDogTestDrive {
 
       one.bark();
       two.bark();
+
+      GoodDog three1 = new GoodDog("spot", "wawa1");
+      GoodDog three2 = new GoodDog("jack", "wawa2");
+     
+      System.out.println(three1.getName() + "," + three1.getSays());
+      System.out.println(three2.getName() + "," + three2.getSays());
+      System.out.println(three1.equals(three2));
+      GoodDog three11 = new GoodDog("spot", "wawa1");
+      GoodDog three3 = three1;
+      System.out.println(three1==three3);
+      System.out.println(three1.equals(three3));
+      System.out.println(three1==three11);
+      System.out.println(three1.equals(three11));
+
+      AB ab = new AB();
+      ab.setAii(8);
+      System.out.println(ab.getAi());
    }
+}
+
+class AA { 
+   private int ai = 3;
+   public void setAii(int aai) {ai = aai;}
+   public int getAi(){return ai;}
+}
+class AB extends AA { 
+   //public void setAi(int aai) {super.setAii(aai);}
 }
