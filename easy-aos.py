5065708b8fbf208908ae1e0c84eda06abb1e9cf6
Chunyun Chen <chunchen@redhat.com>
2016-06-15 15:16:14 +0800

    update

--- easy-aos.py
+++ easy-aos.py
@@ -516,10 +516,8 @@ class AOS(object):
         AOS.do_permission("remove-cluster-role-from-user", "cluster-admin")
 
         dcNum = 4
-        podNum = 3
         if "true" in AOS.enableKibanaOps:
            dcNum += 3
-           podNum += 2
         AOS.resource_validate("oc get dc --no-headers -n {}".format(AOS.osProject), r"(logging-fluentd\s+|logging-kibana\s+|logging-es-\w+|logging-curator\s+|logging-curator-ops\s+|logging-kibana-ops\s+)", dstNum=dcNum)
 
         #if AOS.imageVersion <= "3.2.0" or "latest" in AOS.imageVersion or "v" in AOS.imageVersion:
@@ -532,7 +530,7 @@ class AOS(object):
            AOS.run_ssh_command("oc scale dc/logging-curator-ops --replicas=1", ssh=False)
            AOS.run_ssh_command("oc scale rc/logging-curator-ops-1 --replicas=1", ssh=False)
 
-        AOS.resource_validate("oc get pods -n {}".format(AOS.osProject), r"logging-\w+.+Running", dstNum=podNum)
+        AOS.resource_validate("oc get pods -n {}".format(AOS.osProject), r"logging-\w+.+Running", dstNum=dcNum)
         cprint("Success!","green")
 
     @staticmethod
