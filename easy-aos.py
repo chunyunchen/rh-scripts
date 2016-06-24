777de218aaca295f104d9053e3179b205fb4c65e
Chunyun Chen <chunchen@redhat.com>
2016-06-24 11:33:28 +0800

    update

--- easy-aos.py
+++ easy-aos.py
@@ -505,7 +505,7 @@ class AOS(object):
            AOS.do_permission("add-cluster-role-to-user","cluster-reader",user="system:serviceaccount:{}:aggregated-logging-fluentd".format(AOS.osProject))
            AOS.do_permission("add-scc-to-user","privileged",user="system:serviceaccount:{}:aggregated-logging-fluentd".format(AOS.osProject))
            if AOS.imageVersion == "3.2.1":
-              paraList.extend({'MODE':AOS.deployMode})
+              paraList.extend(AOS.make_para_list({'MODE':AOS.deployMode}))
            #AOS.do_permission("add-cluster-role-to-user","cluster-admin",user="system:serviceaccount:{}:logging-deployer".format(AOS.osProject))
            #AOS.do_permission("add-scc-to-user","hostmount-anyuid",user="system:serviceaccount:{}:aggregated-logging-fluentd".format(AOS.osProject))
 
